8823ba6520463e12a124a67fab300b3961da314c
sserperior <sserperior@gmail.com>
2019-11-16 00:23:20 -0500

    Beef up unit tests for brain and incorporate training right into the tests

--- test/nlp/brain.test.js
+++ test/nlp/brain.test.js
@@ -8,6 +8,10 @@ const { NlpManager } = require('node-nlp');
 const { getManager, threshold } = require('nlp/brain');
 
 describe('brain tests', () => {
+    before(() => {
+        return require('nlp/trainer');
+    });
+
     afterEach(() => {
         sinon.restore();
     });
@@ -31,36 +35,63 @@ describe('brain tests', () => {
         sinon.assert.notCalled(NlpManager.prototype.load);
     });
 
-    it('manager should detect show.hero classification and associated entities within the threshold', () => {
-        return getManager().process('show me Little-John, Valen and Tibertus').then(result => {
-            expect(result.intent).to.equal('show.hero');
+    describe('do.nothing intent tests', () => {
+        const checkDoNothingIntent = (result => {
+            expect(result.intent).to.equal('do.nothing');
             expect(result.score).to.be.at.least(threshold);
-            let entityLjFound = false;
-            let entityValenFound = false;
-            let entityTibsFound = false;
-            result.entities.forEach(entity => {
-                if (entity.entity === 'hero') {
-                    switch (entity.option) {
-                        case 'little john':
-                            entityLjFound = true;
-                            break;
-                        case 'valen':
-                            entityValenFound = true;
-                            break;
-                        case 'tibertus':
-                            entityTibsFound = true;
-                            break;        
-                    }
-                }
-            });
-            expect(entityLjFound).to.be.true;
-            expect(entityValenFound).to.be.true;
-            expect(entityTibsFound).to.be.true;
+        });
+
+        it('no %hero% should result in do.nothing intent', () => {
+            return getManager().process('no Boril').then(checkDoNothingIntent);
+        });
+
+        it('%hero% should result in do.nothing intent', () => {
+            return getManager().process('Bane').then(checkDoNothingIntent);
         });
     });
 
-    it('should find harpoon team member', () => {
-        return getManager().process('What team is werd on?').then(result => {
+    describe('show.hero intent tests', () => {
+        const checkShowHeroIntent = result => {
+            expect(result.intent).to.equal('show.hero');
+                expect(result.score).to.be.at.least(threshold);
+                let entityLjFound = false;
+                let entityValenFound = false;
+                let entityTibsFound = false;
+                result.entities.forEach(entity => {
+                    if (entity.entity === 'hero') {
+                        switch (entity.option) {
+                            case 'little john':
+                                entityLjFound = true;
+                                break;
+                            case 'valen':
+                                entityValenFound = true;
+                                break;
+                            case 'tibertus':
+                                entityTibsFound = true;
+                                break;        
+                        }
+                    }
+                });
+                expect(entityLjFound).to.be.true;
+                expect(entityValenFound).to.be.true;
+                expect(entityTibsFound).to.be.true;
+        };
+
+        it('show %heroes% should be classified as a show.me intent with the necessary entities', () => {
+            return getManager().process('show Little-John, Valen and Tibertus').then(checkShowHeroIntent);
+        });
+
+        it('show me %heroes% should be classified as a show.me intent with the necessary entities', () => {
+            return getManager().process('show me Little-John, Valen and Tibertus').then(checkShowHeroIntent);
+        });
+
+        it('display %heroes% should be classified as a show.me intent with the necessary entities', () => {
+            return getManager().process('display Little-John, Valen and Tibertus').then(checkShowHeroIntent);
+        });
+    });
+
+    describe('harpoon.team.query intent tests', () => {
+        const checkHarpoonTeamQueryIntent = result => {
             expect(result.intent).to.equal('harpoon.team.query');
             expect(result.score).to.be.at.least(threshold);
             let entityWerd5Found = false;
@@ -71,10 +102,89 @@ describe('brain tests', () => {
                 }
             }
             expect(entityWerd5Found).to.be.true;
+        };
+
+        it('which harpoon team is %allianceMember% on should be classified as a harpoon.team.query and the member should be found', () => {
+            return getManager().process('which harpoon team is werd on?').then(checkHarpoonTeamQueryIntent);
+        });
+
+        it('which harpoon team is %allianceMember% part of should be classified as a harpoon.team.query and the member should be found', () => {
+            return getManager().process('which harpoon team is werd part of?').then(checkHarpoonTeamQueryIntent);
+        });
+
+        it('what harpoon team is %allianceMember% on should be classified as a harpoon.team.query and the member should be found', () => {
+            return getManager().process('what harpoon team is werd on?').then(checkHarpoonTeamQueryIntent);
+        });
+
+        it('what harpoon team is %allianceMember% part of should be classified as a harpoon.team.query and the member should be found', () => {
+            return getManager().process('what harpoon team is werd part of?').then(checkHarpoonTeamQueryIntent);
+        });
+
+        it('display harpoon team for %allianceMember% should be classified as a harpoon.team.query and the member should be found', () => {
+            return getManager().process('display harpoon team for werd').then(checkHarpoonTeamQueryIntent);
+        });
+
+        it('display %allianceMember% harpoon team should be classified as a harpoon.team.query and the member should be found', () => {
+            return getManager().process("display werd's harpoon team").then(checkHarpoonTeamQueryIntent);
+        });
+
+        it('show harpoon team for %allianceMember% should be classified as a harpoon.team.query and the member should be found', () => {
+            return getManager().process('show harpoon team for werd').then(checkHarpoonTeamQueryIntent);
         });
     });
 
-    it('unsure why this is broken', () => {
-        return getManager().process("show Boril").then(console.log);
+    describe('show.hero.special intent tests', () => {
+        const checkShowHeroSpecialIntent = result => {
+            expect(result.intent).to.equal('show.hero.special');
+            expect(result.score).to.be.at.least(threshold);
+            let baneFound = false;
+            let valenFound = false;
+            let lJFound = false;
+            let melendorFound = false;
+            result.entities.forEach(entity => {
+                if(entity.entity === 'hero') {
+                    if (entity.option === 'bane') {
+                        baneFound = true;
+                    } else if (entity.option === 'valen') {
+                        valenFound = true;
+                    } else if (entity.option === 'little john') {
+                        lJFound = true;
+                    } else if (entity.option === 'melendor') {
+                        melendorFound = true;
+                    } else {
+                        // Something is wrong.
+                        expect(true, `Unexpected hero ID ${entity.option} found`).to.be.false;
+                    }
+                }
+            });
+            expect(baneFound).to.be.true;
+            expect(valenFound).to.be.true;
+            expect(lJFound).to.be.true;
+            expect(melendorFound).to.be.true;
+        };
+
+        it('what is the special for %hero% should be classified as a show.hero.special intent and the expected entities should be found', () => {
+            return getManager().process('what is the special for Bane, Valen, Little John, and Melendor?').then(checkShowHeroSpecialIntent);
+        });
+
+        it('what is %hero% special should be classified as a show.hero.special intent and the expected entities should be found', () => {
+            return getManager().process("What are Bane's, Valen's, Little John's and Melendor's specials?").then(checkShowHeroSpecialIntent);
+        });
+
+        it('show %hero% special should be classified as a show.hero.special intent and the expected entities should be found', () => {
+            return getManager().process("show Bane's, Valen's, Little John's and Melendor's specials").then(checkShowHeroSpecialIntent);
+        });
+
+        it('show special for %hero% should be classified as a show.hero.special intent and the expected entities should be found', () => {
+            return getManager().process('show specials for Bane, Valen, Little John and Melendor').then(checkShowHeroSpecialIntent);
+        });
+
+        it('display special for %hero% should be classified as a show.hero.special intent and the expected entities should be found', () => {
+            return getManager().process('display specials for Bane, Little John, Melendor and Valen').then(checkShowHeroSpecialIntent);
+        });
+
+        it('display %hero% special should be classified as a show.hero.special intent and the expected entities should be found', () => {
+            return getManager().process("display Bane's, Little John's, Melendor's and Valen's specials").then(checkShowHeroSpecialIntent);
+        });
     });
 });
